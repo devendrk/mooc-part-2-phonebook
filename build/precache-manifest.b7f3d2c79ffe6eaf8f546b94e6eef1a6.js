bd19383921d955c5394fe9b55475b5726f541f88
devendrk <katwaldevendra@hotmail>
2019-07-23 12:19:56 +0300

    deleted wrong ghpage link,

---
+++ build/precache-manifest.b7f3d2c79ffe6eaf8f546b94e6eef1a6.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "edff5288efaa8398c7ba72263547bb3d",
+    "url": "/data-for-countries/index.html"
+  },
+  {
+    "revision": "56862ca13a247a1784a7",
+    "url": "/data-for-countries/static/css/2.22a7d4ef.chunk.css"
+  },
+  {
+    "revision": "a56309d6b6dd0606a936",
+    "url": "/data-for-countries/static/css/main.ecf7ed79.chunk.css"
+  },
+  {
+    "revision": "56862ca13a247a1784a7",
+    "url": "/data-for-countries/static/js/2.f7ffde4a.chunk.js"
+  },
+  {
+    "revision": "a56309d6b6dd0606a936",
+    "url": "/data-for-countries/static/js/main.ab4fdcc2.chunk.js"
+  },
+  {
+    "revision": "cff361559aeca980c9d9",
+    "url": "/data-for-countries/static/js/runtime~main.76cff5e8.js"
+  }
+]);
